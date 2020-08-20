65063c708ac14c887352c2faf614c97862e333ef
Eric Oseid <ericoseid@pop-os.localdomain>
2020-08-19 20:00:03 -0700

    making create account page work

--- pages/CompleteAccount.js
+++ pages/CompleteAccount.js
@@ -1,30 +1,30 @@
-import React from 'react'
+import React from "react";
 
 class CompleteAccount extends React.Component {
   async componentDidMount() {
     const urlParams = new URLSearchParams(window.location.search);
 
-    const authCode = urlParams.get('code');
-    const username = urlParams.get('state').split('=')[1];
+    const authCode = urlParams.get("code");
+    const username = urlParams.get("state").split("=")[1];
 
     const request = {
-      auth_code : authCode,
-      user_name : username
+      username: username,
+      authCode: authCode,
     };
 
-    let response = await fetch('http://127.0.0.1:3005/users/complete', {
-      method : 'POST',
+    let response = await fetch("http://127.0.0.1:3005/users/complete", {
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
-      body: JSON.stringify(request)
+      body: JSON.stringify(request),
     });
 
     console.log(response.status);
   }
 
   render() {
-    return (<p>This page should redirect momentarily...</p>);
+    return <p>This page should redirect momentarily...</p>;
   }
 }
 
--- pages/CreateAccount.js
+++ pages/CreateAccount.js
@@ -1,39 +1,41 @@
-import React from 'react';
-import styles from './CreateAccount.module.css';
-const encode = require('querystring-browser');
+import React from "react";
+import styles from "./CreateAccount.module.css";
+const encode = require("querystring-browser");
 
 class CreateAccount extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = {errorMessage : undefined}
+    this.state = { errorMessage: undefined };
 
     this.onCreateAccount = this.onCreateAccount.bind(this);
     this.validateInputs = this.validateInputs.bind(this);
   }
 
   validateInputs(username, email, password, passwordConfirm) {
-    if (username === '') {
+    if (username === "") {
       this.setState({
-        errorMessage : <p className={styles.error}>Please Enter a Username</p>
+        errorMessage: <p className={styles.error}>Please Enter a Username</p>,
       });
 
       return false;
-    } else if (email === '') {
+    } else if (email === "") {
       this.setState({
-        errorMessage : <p className={styles.error}>Please Enter an email</p>
+        errorMessage: <p className={styles.error}>Please Enter an email</p>,
       });
 
       return false;
-    } else if (password === '' || passwordConfirm === '') {
+    } else if (password === "" || passwordConfirm === "") {
       this.setState({
-        errorMessage : <p className={styles.error}>Please Enter and Confirm your Password</p>
+        errorMessage: (
+          <p className={styles.error}>Please Enter and Confirm your Password</p>
+        ),
       });
 
       return false;
     } else if (password != passwordConfirm) {
       this.setState({
-        errorMessage : <p className={styles.error}>Passwords do not match</p>
+        errorMessage: <p className={styles.error}>Passwords do not match</p>,
       });
 
       return false;
@@ -43,97 +45,116 @@ class CreateAccount extends React.Component {
   }
 
   async onCreateAccount() {
-    this.setState(
-      {errorMessage : undefined}
-    );
+    this.setState();
 
-    const username = document.getElementById('usernameId').value;
-    const email = document.getElementById('emailId').value;
-    const password = document.getElementById('passwordId').value; 
-    const confirmPassword = document.getElementById('confirmPasswordId').value;
+    const username = document.getElementById("usernameId").value;
+    const email = document.getElementById("emailId").value;
+    const password = document.getElementById("passwordId").value;
+    const confirmPassword = document.getElementById("confirmPasswordId").value;
 
     if (!this.validateInputs(username, email, password, confirmPassword)) {
       return;
     }
 
     const request = {
-      user_name : username,
-      user_email : email,
-      user_password : password
-    }
-
-    const response = await fetch('http://127.0.0.1:3005/users/create', {
-      method : 'POST',
-      headers: {
-        'Content-Type': 'application/json',
+      userData: {
+        username: username,
+        userEmail: email,
       },
-      body: JSON.stringify(request)
+      password: password,
+    };
+
+    const response = await fetch("http://127.0.0.1:3005/users/create", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(request),
     });
 
     if (response.ok) {
       const queryState = {
-        user_name : username
+        user_name: username,
       };
 
       const queryParams = {
-        client_id : '90bb1e9b33d9402b887b698376c36715',
-        response_type : 'code',
-        redirect_uri :  'http://127.0.0.1:3000/CompleteAccount',
-        state : encode.stringify(queryState),
-        scope : encode.escape('playlist-modify-public playlist-modify-private')
+        //this has got to be fetched from somewhere rather than statically kept here
+        client_id: "90bb1e9b33d9402b887b698376c36715",
+        response_type: "code",
+        redirect_uri: "http://127.0.0.1:3000/CompleteAccount",
+        state: encode.stringify(queryState),
+        scope: encode.escape("playlist-modify-public playlist-modify-private"),
       };
 
       const paramString = encode.stringify(queryParams);
 
-      window.location.replace(`https://accounts.spotify.com/authorize?${paramString}`);
+      window.location.replace(
+        `https://accounts.spotify.com/authorize?${paramString}`
+      );
     } else {
       if (response.status == 470) {
         this.setState({
-          errorMessage : <p className={styles.error}>Username is Already Taken</p>
+          errorMessage: (
+            <p className={styles.error}>Username is Already Taken</p>
+          ),
         });
       } else if (response.status == 471) {
         this.setState({
-          errorMessage : <p className={styles.error}>Email is Already Taken</p>
+          errorMessage: <p className={styles.error}>Email is Already Taken</p>,
         });
       } else {
         this.setState({
-          errorMessage : <p className={styles.error}>Something went wrong, please try again in some time</p>
+          errorMessage: (
+            <p className={styles.error}>
+              Something went wrong, please try again in some time
+            </p>
+          ),
         });
       }
-    } 
+    }
   }
-  
+
   render() {
     return (
       <div className={styles.main}>
         <style global jsx>
           {`
             body {
-              background-color : Aquamarine;
+              background-color: Aquamarine;
             }
-           `}
+          `}
         </style>
         <h1 className={styles.title}>JamList</h1>
         <br />
-        <label className={styles.labels} htmlFor='username'>Username</label> 
+        <label className={styles.labels} htmlFor="username">
+          Username
+        </label>
         <br />
-        <input type='text' name='username' id='usernameId'/>
+        <input type="text" name="username" id="usernameId" />
         <br />
-        <label className={styles.labels} htmlFor='email'>Email</label> 
+        <label className={styles.labels} htmlFor="email">
+          Email
+        </label>
         <br />
-        <input type='text' name='email' id='emailId'/>
+        <input type="text" name="email" id="emailId" />
         <br />
-        <label className={styles.labels} htmlFor='password'>Password</label> 
+        <label className={styles.labels} htmlFor="password">
+          Password
+        </label>
         <br />
-        <input type='password' name='password' id='passwordId'/>
+        <input type="password" name="password" id="passwordId" />
         <br />
-        <label className={styles.labels} htmlFor='confirmPassword'>Confirm Password</label> 
+        <label className={styles.labels} htmlFor="confirmPassword">
+          Confirm Password
+        </label>
         <br />
-        <input type='password' name='confirmPassword' id='confirmPasswordId'/>
+        <input type="password" name="confirmPassword" id="confirmPasswordId" />
         <br />
         {this.state.errorMessage}
         <br />
-        <button className={styles.createAccount} onClick={this.onCreateAccount}>Create Account</button>
+        <button className={styles.createAccount} onClick={this.onCreateAccount}>
+          Create Account
+        </button>
       </div>
     );
   }
